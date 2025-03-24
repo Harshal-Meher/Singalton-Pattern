9487bd9a52a24defc5d58e4abd5791fd2e8c5fef
Harshal Meher <134125835+Harshal-Meher@users.noreply.github.com>
2025-03-24 14:38:35 +0530

    Update Singleton.java

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,1 +1,23 @@
-public class Singleton {    private volatile static Singleton instance;    private Singleton(){    }    public synchronized static Singleton getInstance() {        if (instance == null) {            synchronized (Singleton.class) {                if (instance == null) {                    instance = new Singleton();                }            }        }        return instance;    }    public static void DisplayMsg() {        System.out.println("Display Message");    }}
+public class Singleton {
+
+    private volatile static Singleton instance;
+
+    private Singleton(){
+
+    }
+
+    public synchronized static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void DisplayMsg() {
+        System.out.println("Display Message");
+    }
+}
